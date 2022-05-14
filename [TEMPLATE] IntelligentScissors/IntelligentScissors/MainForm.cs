f23ba7b607b5f40f00fae94108abe508552f371c
huda <hsamy0706@gmail.com>
2022-05-14 15:22:18 +0200

    Update main form

--- [TEMPLATE] IntelligentScissors/IntelligentScissors/MainForm.cs
+++ [TEMPLATE] IntelligentScissors/IntelligentScissors/MainForm.cs
@@ -14,7 +14,8 @@ namespace IntelligentScissors
         {
             InitializeComponent();
         }
-
+        static List<Point> Anchors;
+        bool moving;
         RGBPixel[,] ImageMatrix;
 
         private void btnOpen_Click(object sender, EventArgs e)
@@ -32,6 +33,7 @@ namespace IntelligentScissors
             }
             txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
             txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+            Anchors = new List<Point>();
   
         }
 
@@ -46,6 +48,60 @@ namespace IntelligentScissors
         private void pictureBox2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                
+                if(pictureBox1.Image != null)
+                {
+                    
+                    int x = e.Location.X;
+                    int y = e.Location.Y;
+                    Point p = new Point(x, y);
+                    int width = ImageOperations.GetWidth(ImageMatrix);
+                    int node = GraphOperation.node_num(x, y, width);
+                    
+                    Shortest_path_op.Dijkstra(node, GraphOperation.graph);
+                    
+                    Anchors.Add(p);
+                    pictureBox1.Refresh();
+                    Console.WriteLine(Anchors.Count);
+                    
+             
+                }
+             
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            int widt = 4;
+            int height = 4;
+            Console.WriteLine("print");
+            if (ImageMatrix != null)
+            {
+                Console.WriteLine("in print");
+                for(int i=0;i<Anchors.Count;i++)
+                {
+                    
+                    e.Graphics.FillEllipse(redBrush, Anchors[i].X, Anchors[i].Y, widt, height);
+                }
+             
+            }
+        
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
         }
     }
 }
